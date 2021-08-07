175fa8ea50334d4082fb835b0ef5df8c0f71fe05
Alex <al12gamer@users.noreply.github.com>
2021-08-07 00:26:45 -0600

    updates

--- fedora-tp.sh
+++ fedora-tp.sh
@@ -2,6 +2,7 @@
 
 PACKAGE_LIST=(
 	vim
+	aha
 	calibre
 	zsh
 	fira-code-fonts
@@ -28,9 +29,11 @@ PACKAGE_LIST=(
 	virtio-win
 	wget
 	java-latest-openjdk
+	java-11-openjdk
 	wine
 	heroic-games-launcher-bin
 	discord
+	linux-util-user
 	
 )
 
@@ -40,6 +43,7 @@ FLATPAK_LIST=(
 	io.lbry.lbry-app
 	org.telegram.desktop
 	com.mojang.Minecraft
+	net.veloren.airshipper
 )
 
 # gnome settings
@@ -99,12 +103,6 @@ for flatpak_name in ${FLATPAK_LIST[@]}; do
 	fi
 done
 
-# grab the pre-5.13 stable Proton release
-echo "Grabbing Proton 5.9-GE-8 and extracting it to the Proton folder here, please move this to your new steam compatibility tools folder"
-sleep 1
-wget https://github.com/GloriousEggroll/proton-ge-custom/releases/download/5.9-GE-8-ST/Proton-5.9-GE-8-ST.tar.gz
-mkdir ~/Proton
-tar -xvf Proton-5.9-GE-8-ST.tar.gz ~/Proton
 
 # add ohmyzsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
